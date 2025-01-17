607770b99abbf5a7fdcf6c41dcc72df07765cd2c
zaybournand <zaybournand@gmail.com>
2025-01-16 23:09:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
-from flask import Flask
+from flask import Flask, url_for, request, redirect, render_template, flash
+from flask_migrate import Migrate 
+from flask_sqlalchemy import SQLAlchemy 
+from flask_login import LoginManager, login_user, logout_user, current_user, UserMixin, login_required
+from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+from flask_bcrypt import Bcrypt
+
 
 app = Flask(__name__)
 
